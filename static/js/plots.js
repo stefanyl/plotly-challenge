a7b0365d8555f1ccd92fb969d2a488eb5e388a38
Stefany Lima <limastefany900@gmail.com>
2021-10-12 00:49:58 -0500

    renamed plots.js and commented

---
+++ static/js/plots.js
@@ -0,0 +1,20 @@
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+//Use d3 to read in json file
+//d3.json("samples.json").then((data) 
+
+// Grab values from the response json object to build the plots
+// Sort data
+    //var sortedNames = data.sort((a, b) => b.names - a.names);
+
+//Slice the first ten objects for chart. Reverse to accommodate Plotly's defaults.
+    //slicedData = sortedNames.slice(0 ,10);
+    //reversedData = slicedData.reverse();
+
+//Create a bubble chart that displays each sample
+
+//Display the sample metadata i.e. an individual's demographic info
+
+//Display each key-value pair from the metadata json object
+
+//Update all the plots any time a new sample is selected 
